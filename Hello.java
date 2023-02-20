6685d88387fe54b4e1382ed60791bd020010ddb0
Satvik gupta <124979868+Satvik-knolx@users.noreply.github.com>
2023-02-20 20:41:48 +0530

    commit to show the output in jenkins

---
+++ Hello.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Hello {
+  //creating this java file so that after commit its output will be automatically shown in jenkins console
+  public static void main(String args[]){
+    System.out.println("This output is auto triggered after commit in github into jenkins console");
+  }
+}
